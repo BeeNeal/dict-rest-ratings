2d6ef3b77fc3f2f3214eb8d5084e0efbe0adc512
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-09 15:44:43 -0700

    further study, @wip

--- ratings.py
+++ ratings.py
@@ -1,17 +1,70 @@
 """Restaurant rating lister."""
 
+import random
+
 
 def get_restaurant_ratings(filename):
-    """opens text file and prints the Restaurants (key)
-    and ratings (value) sorted and return nothing"""
+    """opens text file and prints the restaurants and ratings sorted
+    Returns nothing"""
+
     ratings = {}
     with open(filename) as text_file:
         for line in text_file:
-            rest_data = line.rstrip().split(':')
-            ratings[rest_data[0]] = rest_data[1]
+            name, score = line.rstrip().split(':')
+            ratings[name] = score
+    return ratings
+
+
+def print_sorted_ratings(ratings):
+    """prints the restaurants and ratings sorted and returns nothing"""
+
     for restaurant, rating in sorted(ratings.items()):
         print "{name} is rated at {score}.".format(
             name=restaurant, score=rating)
 
 
-get_restaurant_ratings("scores.txt")
+def prompt_user_for_rating(orig_ratings):
+    """adding user name and score to existing ratings"""
+
+    new_ratings = orig_ratings
+    user_rest = raw_input("Please enter a restaurant name: \n")
+    user_rest = user_rest.title()
+    while True:
+        user_score = raw_input("Please enter that restaurant's score (1-5): \n")
+        try:
+            user_score = int(user_score)
+        except ValueError:
+            print "Try again - you did not enter a number from 1 - 5."
+            continue
+        if user_score not in range(1, 6):
+            print "Try again - you did not enter a number from 1 - 5."
+            continue
+        break
+
+    new_ratings[user_rest] = user_score
+    return new_ratings
+
+
+def setup_interaction():
+    """set up the initial interaction, doesn't take in any parameters
+    Runs functions based on user input"""
+    current_ratings = get_restaurant_ratings("scores.txt")
+    while True:
+        print "You can get restaurant ratings, add a restaurant, update random restaurant, or quit."
+        user_choice = raw_input("Enter P to print, A to add, R to update random restaurant or Q to quit:\n")
+        if user_choice.lower().startswith('p'):
+            print_sorted_ratings(current_ratings)
+        elif user_choice.lower().startswith('a'):
+            current_ratings = prompt_user_for_rating(current_ratings)
+        elif user_choice.lower().startswith('r'):
+            current_ratings = random_update(current_ratings)
+        else:
+            break
+
+
+def random_update(ratings):
+    """in progress"""
+    random_rest = random.choice(ratings)
+    print random_rest
+
+setup_interaction()
